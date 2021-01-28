4c43efa056f0392ca4f731551d33e6680505e558
Will Clarkson <wiclarks@umich.edu>
2021-01-28 16:48:57 -0500

    Updated endToEnd for running on sciserver or Datalab

--- python/endtoend.py
+++ python/endtoend.py
@@ -12,22 +12,54 @@ import fomStatic
 import mapRead
 import calcFOM
 
-def runSeveral(nside=128, nMax=3):
+def runSeveral(nside=128, nMax=3, sciserver=True, \
+                   lSims=['baseline_2snaps_v1.5_10yrs.db', \
+                              'bulges_cadence_bulge_wfd_v1.5_10yrs.db'], \
+                   filSims='NONE', fileHasFullPaths=True):
 
-    """Convenience-wrapper to run on several opsims"""
+    """Convenience-wrapper to run on several opsims. The
+    sciserver=True sets the opsim path appropriately. 
+
+    To specify input lists: either specify a list of filenames as the
+    input argument lSims, or populate filSims with one line per
+    database. If the file has been given, its contents supersede the
+    contents of lSims."""
+
+    ## 2021-01-28 update: set the root directory rather than copying
+    ## the opsims in here.
+    if sciserver:
+        dbroot = '/home/idies/workspace/lsst_cadence/FBS_1.5/'
+    else:
+        dbroot = '/sims_maf/fbs_1.5/short_exp/'
+
+    # 2021-01-28 WIC: There are so many opsims that it's not
+    # immediately clear to me how pass the inputs in correctly. I
+    # suspect a file is going to be the way forward. For the moment,
+    # though, we will also set a default entry.
 
     # by default, runs on all the .db files in the current working
     # directory
-    lDb = glob.glob("*1.4*.db")
+    # lDb = glob.glob("*1.4*.db")
+
+    # read the file list from file if given. This list may well have
+    # leading paths.
+    if os.access(filSims, os.R_OK):
+        with open(filSims, 'r') as rObj:
+            lSims = rObj.read().splitlines()
+
+        # If the file already lists the full paths to the opsims, then
+        # we don't need to add it to the individual paths.
+        if fileHasFullPaths:
+            dbroot = ''
 
     # how many do we run?
     if nMax < 0:
-        iMax = len(lDb)
+        iMax = len(lSims)
     else:
-        iMax = min([nMax, len(lDb)])
+        iMax = min([nMax, len(lSims)])
 
     for iDb in range(iMax):
-        thisDb = lDb[iDb]
+        thisDb = "%s%s" % (dbroot, lSims[iDb])
         go(thisDb, nside=nside)
 
 def go(dbFil='baseline_v1.4_10yrs.db', nside=128, \
